2174ecded531aa97570458e4f71c04fe6bd932cf
MarianVytak <marianvytak@gmail.com>
2020-03-07 10:35:44 +0200

    Start Work!

---
+++ src/assets/js/components/customs.js
@@ -0,0 +1,40 @@
+$(function () {
+
+    'use strict';
+
+
+
+    // $('.section-1__slider_card').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     infinite: true,
+    //     focusOnSelect: true,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //     fade: false,
+    //     arrows: false,
+    //     dots: true,
+    //     asNavFor: '.section-1__slider_img',
+    //     appendDots: $('.section-1__slider__counter'),
+    //     customPaging: function (slider, i) {
+    //         return '<span class="slide-current">' + (i + 1) + '</span>' + '<span>из</span>' + '<span class="slide-length">' + slider.slideCount +'</span>';
+    //     },
+    //     responsive: [
+    //         {
+    //             breakpoint: 9999,
+    //             settings: "unslick"
+    //         },
+    //         {
+    //             breakpoint: 1200,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1,
+    //                 arrows: true,
+    //             }
+    //         }
+    //     ]
+    // });
+
+
+
+});
