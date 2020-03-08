37f577d484dcd5c6f39505d2fed33b274bfe6865
MarianVytak <marianvytak@gmail.com>
2020-03-08 17:35:23 +0200

    Added Navigation anchors.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -1,2 +1,19 @@
 // Lightgallery
 lightGallery(document.getElementById('lightgallery'));
+
+
+// Navigation anchors
+const anchors = document.querySelectorAll('.navigation__link')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
