8f8ea2fa9c635a0c775ddccf0cb9ab9e028b4d19
MarianVytak <marianvytak@gmail.com>
2020-03-09 09:36:54 +0200

    Added Form send messages.

--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
 
-	$(".form").submit(function() {
+	$("#s1-form").submit(function() {
 		$.ajax({
 			type: "POST",
-			url: "mail.php",
+			url: "mail-1.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$(".form").trigger("reset");
+		});
+		return false;
+	});
+
+	$("#s8-form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail-2.php",
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
